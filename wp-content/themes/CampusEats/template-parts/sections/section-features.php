07b99a7639bad6e6ac8b4ec26d444a6a6b4c0c24
jaimin pandya <jaiminmoslake7020@gmail.com>
2020-03-25 18:07:01 -0700

    Js Update

--- wp-content/themes/CampusEats/template-parts/sections/section-features.php
+++ wp-content/themes/CampusEats/template-parts/sections/section-features.php
@@ -3,25 +3,25 @@
     <h3 class=" section-title">Features</h3>
     <div class="sub-section">
         <div class="feature-box">
-			<?php
+            <?php
 
-			$args = array(
-				'category_name' => 'Features',
-				'orderby'       => ''
-			);
-			$features_query = new WP_Query( $args );
+            $args = array(
+                'category_name' => 'Features',
+                'orderby' => ''
+            );
+            $features_query = new WP_Query($args);
 
-			$i = 0;
-			if ( $features_query->have_posts() ) :
-				while ( $features_query->have_posts() ) :
-					$features_query->the_post();
+            $i = 0;
+            if ($features_query->have_posts()) :
+                while ($features_query->have_posts()) :
+                    $features_query->the_post();
 
-					$activeCLass = "";
-					if ( $i == 0 ) {
-						$activeCLass = ' active ';
-					}
-					?>
-                    <div class="features feature-<?= ( $i + 1 ); ?> <?php implode( '  ', get_post_class() ); ?> ">
+                    $activeCLass = "";
+                    if ($i == 0) {
+                        $activeCLass = ' active ';
+                    }
+                    ?>
+                    <div <?php post_class(' features feature-' . ($i + 1) . ' ', get_the_ID()); ?> >
                         <div class="icon-box ">
                             <img src="<?php the_post_thumbnail_url(); ?>"
                                  alt="<?php the_title(); ?>">
@@ -31,12 +31,11 @@
                             <p class=" feature-text "><?php the_content(); ?></p>
                         </div>
                     </div>
-					<?php
-					$i ++;
-				endwhile;
-			endif;
-			?>
+                    <?php
+                    $i++;
+                endwhile;
+            endif;
+            ?>
         </div>
-    </div>
     </div>
 </section>
--- wp-content/themes/CampusEats/template-parts/sections/section-needs.php
+++ wp-content/themes/CampusEats/template-parts/sections/section-needs.php
@@ -1,39 +1,39 @@
 <?php ?>
-<section id="needs-content" class="needs main-section  " >
-    <section class="sub-section" >
-        <p class="question" >Why CampusEats?</p>
+<section id="needs-content" class="needs main-section  ">
+    <section class="sub-section">
+        <p class="question">Why CampusEats?</p>
         <div class="answer-box">
-	        <?php
+            <?php
 
-                $args = array (
-                    'category_name'     => 'Needs',
-                    'orderby' => ''
-                );
-                $needs_query = new WP_Query( $args );
+            $args = array(
+                'category_name' => 'Needs',
+                'orderby' => ''
+            );
+            $needs_query = new WP_Query($args);
 
-                $i = 0 ;
-                if ($needs_query->have_posts() ) :
-                    while ( $needs_query->have_posts() ) :
-                        $needs_query->the_post();
+            $i = 0;
+            if ($needs_query->have_posts()) :
+                while ($needs_query->have_posts()) :
+                    $needs_query->the_post();
 
-                        $activeCLass = "" ;
-                        if( $i == 0 ){
-                            $activeCLass = ' active ' ;
-                        }
-                        ?>
-                        <div class="answers answer-<?= ($i+1); ?> <?php implode('  ', get_post_class());  ?> " >
-                            <div class="icon-box" >
-                                <i class=" fas fa-search "></i>
-                            </div>
-                            <div class="text-info">
-                                <p class=" answer-text " ><?php the_content(); ?></p>
-                            </div>
+                    $activeCLass = "";
+                    if ($i == 0) {
+                        $activeCLass = ' active ';
+                    }
+                    ?>
+                    <div <?php post_class(' answers answer-' . ($i + 1) . ' ', get_the_ID()); ?> >
+                        <div class="icon-box">
+                            <i class=" fas fa-search "></i>
                         </div>
-                        <?php
-                        $i++;
-                    endwhile;
-                endif;
-	        ?>
+                        <div class="text-info">
+                            <p class=" answer-text "><?php the_content(); ?></p>
+                        </div>
+                    </div>
+                    <?php
+                    $i++;
+                endwhile;
+            endif;
+            ?>
         </div>
     </section>
 </section>
